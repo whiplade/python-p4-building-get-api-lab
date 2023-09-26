635d9ac6e6c5b659933e9eaa3cee8911176994f2
whiplade <nmawira13@gmail.com>
2023-09-26 18:21:21 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,38 +1,63 @@
-#!/usr/bin/env python3
-
+import json
+from urllib import response
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
 
-@app.route('/bakeries/<int:id>')
+    return jsonify(bakery_list)
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery:
+        bakery_dict = bakery.to_dict()
+        return jsonify(bakery_dict)
 
-@app.route('/baked_goods/by_price')
+    else:
+        return jsonify({"message": "Bakery not found"}), 404
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [good.to_dict() for good in baked_goods]
 
-@app.route('/baked_goods/most_expensive')
+    return jsonify(baked_goods_list)
+
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    luxurious_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if luxurious_good:
+        luxurious_good_dict = luxurious_good.to_dict()
+        return jsonify(luxurious_good_dict)
+    else:
+        return jsonify({"message": "No baked goods found"}), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,20 +1,59 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import ForeignKey, MetaData, Integer
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Integer
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
+    __tablename__ = "bakeries"
+
+    serialize_rules = "+baked_goods,"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    # the relationship to BakedGood
+    baked_goods = db.relationship("BakedGood", back_populates="bakery")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            "baked_goods": [good.to_dict() for good in self.baked_goods],
+        }
+
 
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
 
-    id = db.Column(db.Integer, primary_key=True)
-    
+    serialize_rules = "-bakery,"
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    price = db.Column(db.Integer())
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery_id = db.Column(Integer(), ForeignKey("bakeries.id"))
+
+    # the relationship to Bakery
+    bakery = db.relationship("Bakery", back_populates="baked_goods")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "created_at": self.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            "updated_at": self.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+            "bakery_id": self.bakery_id,
+        }
